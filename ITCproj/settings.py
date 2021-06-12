bc21a286d873a25e8461512b9b713b927d991266
Saksham <20D170037@iitb.ac.in>
2021-06-12 12:03:13 +0530

    more fixes

--- ITCproj/settings.py
+++ ITCproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@dtm5$k+a-#w82hh&$3(yj&ts$mbzgo^1)xr(ut!4gb9qx%&4h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sakshamoff.pythonanywhere.com', '127:0:0:1',]
+ALLOWED_HOSTS = ['sakshamoff.pythonanywhere.com', '127.0.0.1',]
 
 
 # Application definition
