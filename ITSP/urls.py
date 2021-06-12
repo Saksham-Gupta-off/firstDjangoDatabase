1d343dd9aa6ceed6f1e5c1faf4e3f321c062cdcc
Saksham <20D170037@iitb.ac.in>
2021-06-12 20:58:06 +0530

    some minor changes

--- ITSP/urls.py
+++ ITSP/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('../API', views.index, name='API'),
     path('<str:teamid>', views.userInfo, name='idpage')
 ]
