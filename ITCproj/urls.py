7557e97644e7965a5bce1e0179bcb0a3067d6a54
Saksham <20D170037@iitb.ac.in>
2021-06-12 11:32:04 +0530

    adding the Html

--- ITCproj/urls.py
+++ ITCproj/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('ITSP/', include('ITSP.urls')),
+    path('', include('ITSP.urls')),
     path('admin/', admin.site.urls),
 ]
--- ITSP/views.py
+++ ITSP/views.py
@@ -4,6 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello there")
-def index1(request):
-    return HttpResponse("Hello a there")
+    return render(request, 'ITSP/index.html', {})
